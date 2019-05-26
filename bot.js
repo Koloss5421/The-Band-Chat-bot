d6ab062768537193c1c459cdbb7361cfd0dc808f
Koloss <jphardin93@gmail.com>
2019-05-26 05:36:46 -0500

    Added channel presence and joining voice chat.

--- bot.js
+++ bot.js
@@ -3,7 +3,9 @@ var logger = require('winston');
 var auth = require('./auth.json');
 
 var kolossID = "342162894873427979";
-var speakeasyID = "582145740705628170";
+var speakeasyID = "582080751542075393";
+var controlChannel = "582145740705628170";
+var presenceString = "Smooth Jazz";
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -19,13 +21,17 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Setting Presence: ' + presenceString);
+    bot.setPresence(presenceString);
+    logger.info('Joining Speakeasy: ' + speakeasyID);
+    bot.joinVoiceChannel(speakeasyID);
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (channelID == speakeasyID) {
-      if (userID == kolossID ) {
         if (message.substring(0, 1) == '!') {
+            // Only accept commands from the control server
+            if (channelID == controlChannel) {
             var args = message.substring(1).split(' ');
             var cmd = args[0];
 
@@ -41,12 +47,5 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 // Just add any case commands if you want to..
              }
          }
-       } else {
-         bot.sendMessage({
-             to: channelID,
-             message: "I'm sorry. You do not own this Speakeasy."
-         });
-       }
-       logger.info('[' + user + '(' + userID + ')]' + ' attempted to use command "' + args[0] + '" in channel: ' + channelID );
      }
 });
