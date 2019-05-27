0ac40d4d0b24cf3f05c1f675565ebfd2bd80a0c7
Koloss <jphardin93@gmail.com>
2019-05-27 06:24:57 -0500

    Defang URL to prevent help from triggering a video resolve in chat when help is used. Made help a multiline message rather than multiple messages, added breaks to the case statement.

--- bot.js
+++ bot.js
@@ -60,15 +60,19 @@ bot.on('message', function(message) {
             case 'gohome':
                 message.channel.send("Returning to the speakeasy...");
                 joinVoiceChannel();
+                break;
             case 'help':
-                message.channel.send("The band is pretty simple. We play youtube videos.");
-                message.channel.send("=====       Commands:       =====");
-                message.channel.send("~add [url]    - Add a song to the playlist. Ex. !add http://www.youtube.com/watch?v=Evb31p5vFs4");
-                message.channel.send("~skip         - Skip the current playing song - or use to get the bot going again.");
-                message.channel.send("~getband      - Call the band to your current voice channel. All you have to do is be in there.");
-                message.channel.send("~gohome       - We get the message. We simply go back to the speakeasy for all to hear when the chats are packed.");
+                message.channel.send(`
+                The band is pretty simple. We play youtube videos.
+                =====       Commands:       =====");
+                ~add [url]    - Add a song to the playlist. Ex. ~add hxxp://www.youtube.com/watch?v=Evb31p5vFs4
+                ~skip         - Skip the current playing song - or use to get the bot going again.");
+                ~getband      - Call the band to your current voice channel. All you have to do is be in there.");
+                ~gohome       - We get the message. We simply go back to the speakeasy for all to hear when the chats are packed.`);
+                break;
             default:
                 message.channel.send("That command does not exist: " + message.content);
+                break;
         }
     }
 });
